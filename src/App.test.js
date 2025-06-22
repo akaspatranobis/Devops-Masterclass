5c8eaec42f99905a8228228a4aca5e12e0dd9827
Akas Patranobis <akaspatranobis@yahoo.com>
2025-06-22 18:46:49 +0530

    up

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Subscribe button', () => {
-  render(<App />);
-  const btnElement = screen.getByText(/Subscribe to Watch/i);
-  expect(btnElement).toBeInTheDocument();
-});
